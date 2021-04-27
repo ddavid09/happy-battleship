67cfb759aa440b81a785fd6461e611eb7070d3e6
Dominik Dawidziak <ddawidziak.dd@gmail.com>
2021-04-27 19:59:35 +0200

    removed comments in startup

--- HappyBattleship/HappyBattleship.web/Startup.cs
+++ HappyBattleship/HappyBattleship.web/Startup.cs
@@ -12,8 +12,6 @@ namespace HappyBattleship.Web
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddCors(options =>
@@ -47,7 +45,6 @@ namespace HappyBattleship.Web
 
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
